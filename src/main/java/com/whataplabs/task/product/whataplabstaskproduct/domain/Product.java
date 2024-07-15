a2d9acc8529423310bc91fe746c732026d04b5bc
hanbee <sonhanbi1002@gmail.com>
2024-07-15 22:15:45 +0900

    feat: product 도메인 생성

---
+++ src/main/java/com/whataplabs/task/product/whataplabstaskproduct/domain/Product.java
@@ -0,0 +1,10 @@
+package com.whataplabs.task.product.whataplabstaskproduct.domain;
+
+import java.math.BigDecimal;
+
+public class Product {
+    private Long id;
+    private String name;
+    private BigDecimal price;
+    private int amount;
+}
